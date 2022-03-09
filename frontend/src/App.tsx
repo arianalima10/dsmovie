2f99048fbd656dd8403957dc5d24b3a81d299ae1
Ariana Lima <ariana.crodrigues@gmail.com>
2022-03-09 11:58:50 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,8 @@
-import {ReactComponent as GithubIcon} from './assets/img/github.svg';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-	<header>
-	 <nav>
-	  <div>
-	   <h1>DSMovie</h1>
-
-	   <a href="https://github.com/arianalima10">
-	   <div>
-		   <GithubIcon />
-			<p>/arianalima10</p>
-	   </div>
-	   </a>
-	  </div>
-	 </nav>
-	</header>	
+	<Navbar />
   );
 }
 
